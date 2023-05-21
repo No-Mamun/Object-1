c508b498dfe471f743f9c34680edc002a9a21a78
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2023-05-21 08:18:15 +0600

    Add files via upload
    Object :1

---
+++ main.js
@@ -0,0 +1,146 @@
+// 019 | Object & Object Constructor | The Modern JavaScript Tutorial
+
+/*
+// No:1
+const user = {
+id:123,
+name:"Mamun",
+age:35,
+              }
+// console.log(user)
+console.log(user['age']);
+
+*/
+
+/*
+// No:2
+
+const user = {}
+
+user.id=123;
+user.name=123;
+user.age=123;
+
+console.log(user);
+
+*/
+
+// No:3
+
+/*
+const user = {
+              id:123,
+              name:"Mamun",
+              age:35,
+                            }
+
+user.address = "Dhaka";
+
+              console.log(user);
+*/
+// No:4
+/*
+const car = {
+
+name: "Fite",
+
+model: 500,
+
+weight:"850kg",
+
+color:"White",
+
+start:function (){
+
+console.log("car has Started");
+
+},
+
+drive:function () {
+
+console.log("car is driving");
+
+},
+
+};
+
+// console.log(car.weight);
+
+console.log (car["model"]);
+
+*/
+
+// No:5
+
+/*
+const car = {
+
+name: "Fite",
+
+model: 500,
+
+weight:"850kg",
+
+color:"White",
+
+start:function (){
+
+// this.drive();
+
+console.log("car has Started");
+
+},
+
+drive:function () {
+
+console.log("car is driving Mamun Khan");
+
+},
+
+};
+
+console.log(car.weight);
+
+console.log (car["model"]);
+
+car.start ();
+
+car.drive ();
+*/
+
+
+// No:6
+
+const car = {
+
+name: "Fite",
+
+model: 500,
+
+weight:"850kg",
+
+color:"White",
+
+start:function (){
+
+// this.drive();
+
+console.log("car has Started");
+
+},
+
+drive:function () {
+
+console.log("car is driving Mamun Khan");
+
+},
+
+};
+
+console.log(car.weight);
+
+console.log (car["model"]);
+
+car.start ();
+
+car.drive ();
